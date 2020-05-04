b45b0e589a41c7f7bf6fec4c16a6be7cbf1d621a
vjeuel <vjeuel@gmail.com>
2020-05-04 14:03:33 -0500

    reduced html file having all the divs, input and button on js file

--- javascript/script.js
+++ javascript/script.js
@@ -1,50 +1,72 @@
 // Variables
-var containerCl = $(".container");
-var sectionTag = $("section");
-var hourCl = $(".hour");
-var timeBlockCl = $(".time-block");
-var inputTaskCl = $(".input-task");
+var containerCl = $(".container")
 var inputTag = $("input");
-var saveTaskBt = $(".save-task")
-
 var time = moment().format("H");
 
+
+
 // Variables of Arrays
-// var timeArr = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
+var timeArr = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
+var hourArr = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
 
 // Executes when the DOM is fully loaded
-$(document).ready(function () {
-   // Date on top of page
-   $("#currentDay").append(moment().format("MMMM Do YYYY"));
+// function storeTime() {
+   //    localStorage.meetingTime = $("#schedule").val();
+   // };
    
-   // Input placeholder
-   inputTag.attr("placeholder", "now what?");
-   
-   console.log("test");
-   inputTaskCl.each(function(block) {
-      var timeOfDay = $("input")[block].timeOfDay;
-      var blockTime = parseInt($(inputTaskCl + timeOfDay).attr("data-hour"));
+   $(document).ready(function () {
       
-      var value = localStorage.getItem(timeOfDay);
+      $.each(timeArr, function (index, value) {
+         containerCl.append('<section class="row">\
+                        <div class="col-2 hour">' + value + '</div>\
+                        <div class="col-8 time-block">\
+                        <input class="input-task form-control form-control-lg" data-hour="' + value + '" type="text" placeholder="now what?"/></div>\
+                        <div class="col-2"><button class="btn saveBtn save-task" type="button">save</button></div></section>');
       
-      $(inputTaskCl + timeOfDay).val(value);
-      if (blockTime < time) {
-         $(inputTaskCl + timeOfDay).addClass("past");
-      } else if (blockTime == time) {
-         $(inputTaskCl + timeOfDay).addClass("current");
-      } else {
-         $(inputTaskCl + timeOfDay).addClass("future");
-      };
+                        
+         inputTag.attr("data-hour", hourArr)
+      console.log(inputTag);
    });
+   
+                     
+   
+   
+   //data-hour Array to be inserted later -  ' + hourArr[i] + '
 
-   // Save button
-   $("button").each(function(block) {
-      $("button")[block].onclick = function () {
-         var input = this.timeOfDay.substring(0, this.timeOfDay.length - 5);
-         localStorage.setItem(input, $(saveTaskBt + input).val());
-      };
-   });
+   // // Date on top of page
+   // $("#currentDay").append(moment().format("MMMM Do YYYY"));
+   
+   // // Input placeholder
+   // inputTag.attr("placeholder", "now what?");
+   
+   // inputTaskCl.each(block => {
+   //    var timeOfDay = $("input")[block].timeOfDay;
+   //    var blockTime = parseInt($("." + timeOfDay).attr("data-hour"));
+      
+   //    var value = localStorage.getItem(timeOfDay);
+      
+   //    $("." + timeOfDay).val(value);
+   //    if (blockTime < time) {
+   //       $("." + timeOfDay).addClass("past");
+   //    } else if (blockTime === time) {
+   //       $("." + timeOfDay).addClass("current");
+   //    } else {
+   //       $("." + timeOfDay).addClass("future");
+   //    };
+      
+      
+   //    console.log("test");
+   // });
+   // // Save button
+   // $("button").each(block => {
+   //    $("button")[block].onclick = function () {
+   //       var input = this.timeOfDay.substring(0, this.timeOfDay.length - 1);
+   //       localStorage.setItem(input, $(saveTaskBt + input).val());
+   //    };
+   // });
 });
+
+
 
 
 
@@ -53,5 +75,5 @@ $(document).ready(function () {
 //    }
 
 //   for(i = 0; i < timeArr.length; i++) {
-//     hourCl.append('<div id="div">' + timeArr[i] + '</div>');
+//     hourCl.append('<div id="div">' + timeArr[i] + '</>');
 //   };
