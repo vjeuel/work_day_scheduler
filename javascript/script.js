e78869e8cff2b5a5f42c0372dda31bf80d8bcd48
vjeuel <vjeuel@gmail.com>
2020-05-04 00:53:52 -0500

    styling to rest

--- javascript/script.js
+++ javascript/script.js
@@ -5,9 +5,10 @@ var hourCl = $(".hour");
 var timeBlockCl = $(".time-block");
 var inputTaskCl = $(".input-task");
 var saveTaskBt = $(".save-task")
+var time = moment().format("H");
 
 // Variables of Arrays
-var timeArr = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
+// var timeArr = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
 
 
 // Executes when the DOM is fully loaded
@@ -16,13 +17,14 @@ $(document).ready(function () {
    $("#currentDay").append(moment().format("MMMM Do YYYY"));
 
 
-   for (let i = 0; i < timeArr.length; i++) {
-      containerCl.append(mainTag);
-   }
-
-  for(i = 0; i < timeArr.length; i++) {
-    hourCl.append('<div id="div">' + timeArr[i] + '</div>');
-  }
-
-
+   
+   
 });
+
+//    for (let i = 0; i < timeArr.length; i++) {
+//       containerCl.append(mainTag);
+//    }
+
+//   for(i = 0; i < timeArr.length; i++) {
+//     hourCl.append('<div id="div">' + timeArr[i] + '</div>');
+//   }
