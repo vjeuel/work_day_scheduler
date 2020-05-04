5f4528fde8fbf22ebf67aa392e887348c35bb677
vjeuel <vjeuel@gmail.com>
2020-05-04 18:00:49 -0500

    starting over

--- javascript/script.js
+++ javascript/script.js
@@ -3,77 +3,20 @@ var containerCl = $(".container")
 var inputTag = $("input");
 var time = moment().format("H");
 
-
-
 // Variables of Arrays
 var timeArr = ["8am", "9am", "10am", "11am", "12pm", "1pm", "2pm", "3pm", "4pm", "5pm", "6pm"];
 var hourArr = ["8", "9", "10", "11", "12", "13", "14", "15", "16", "17", "18"];
 
 // Executes when the DOM is fully loaded
-// function storeTime() {
-   //    localStorage.meetingTime = $("#schedule").val();
-   // };
-   
-   $(document).ready(function () {
+$(document).ready(function () {
+   $("#currentDay").append(moment().format('MMMM Do YYYY -  ha'));
       
-      $.each(timeArr, function (index, value) {
-         containerCl.append('<section class="row">\
-                        <div class="col-2 hour">' + value + '</div>\
-                        <div class="col-8 time-block">\
-                        <input class="input-task form-control form-control-lg" data-hour="' + value + '" type="text" placeholder="now what?"/></div>\
-                        <div class="col-2"><button class="btn saveBtn save-task" type="button">save</button></div></section>');
-      
-                        
-         inputTag.attr("data-hour", hourArr)
-      console.log(inputTag);
+   $.each(timeArr, function (index, value) {
+      containerCl.append('<section class="row">\
+                     <div class="col-2 hour">' + value + '</div>\
+                     <div class="col-8 time-block">\
+                     <input id="input-data" class="input-task form-control form-control-lg" data-hour="' + value + '" type="text" placeholder="what now?"/></div>\
+                     <div class="col-2"><button id="' + value + '" class="btn saveBtn save-task" type="button">save</button></div></section>');   
    });
-   
-                     
-   
-   
-   //data-hour Array to be inserted later -  ' + hourArr[i] + '
-
-   // // Date on top of page
-   // $("#currentDay").append(moment().format("MMMM Do YYYY"));
-   
-   // // Input placeholder
-   // inputTag.attr("placeholder", "now what?");
-   
-   // inputTaskCl.each(block => {
-   //    var timeOfDay = $("input")[block].timeOfDay;
-   //    var blockTime = parseInt($("." + timeOfDay).attr("data-hour"));
-      
-   //    var value = localStorage.getItem(timeOfDay);
-      
-   //    $("." + timeOfDay).val(value);
-   //    if (blockTime < time) {
-   //       $("." + timeOfDay).addClass("past");
-   //    } else if (blockTime === time) {
-   //       $("." + timeOfDay).addClass("current");
-   //    } else {
-   //       $("." + timeOfDay).addClass("future");
-   //    };
-      
-      
-   //    console.log("test");
-   // });
-   // // Save button
-   // $("button").each(block => {
-   //    $("button")[block].onclick = function () {
-   //       var input = this.timeOfDay.substring(0, this.timeOfDay.length - 1);
-   //       localStorage.setItem(input, $(saveTaskBt + input).val());
-   //    };
-   // });
+                  
 });
-
-
-
-
-
-//    for (let i = 0; i < timeArr.length; i++) {
-//       containerCl.append(mainTag);
-//    }
-
-//   for(i = 0; i < timeArr.length; i++) {
-//     hourCl.append('<div id="div">' + timeArr[i] + '</>');
-//   };
